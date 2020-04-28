a7ea814412a3d9d900c8102ad8c9f77e89dffa39
Will Kastgit config --global user.name Will <william.kast@gmail.com>
2020-04-28 19:02:45 -0400

    Exercise 1.2

--- Part1/courseinfo/src/index.js
+++ Part1/courseinfo/src/index.js
@@ -7,9 +7,19 @@ const Header = (props) => {
   )
 }
 
-const Content = (props) => {
+const Parts = (props) => {
   return(
     <p> {props.part} {props.exercise} </p>
+  )
+}
+
+const Content = (props) => {
+  return(
+    <div>
+      <Parts part={props.part1} exercise={props.exercises1} />
+      <Parts part={props.part2} exercise={props.exercises2} />
+      <Parts part={props.part3} exercise={props.exercises3} />
+    </div>
   )
 }
 
@@ -31,9 +41,7 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part = {part1} exercise = {exercises1} />
-      <Content part = {part2} exercise = {exercises2} />
-      <Content part = {part3} exercise = {exercises3} />
+      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3}/>
       <Total exercises = {exercises1 + exercises2 + exercises3} />
     </div>
   )
