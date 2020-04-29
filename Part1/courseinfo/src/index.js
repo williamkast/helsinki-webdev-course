a696a56c44af1f5a1cef6b6a41ce6109be00950d
Will Kastgit config --global user.name Will <william.kast@gmail.com>
2020-04-29 10:00:02 -0400

    Exercise 1.3

--- Part1/courseinfo/src/index.js
+++ Part1/courseinfo/src/index.js
@@ -2,23 +2,26 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const Header = (props) => {
+  console.log(props)
   return(
     <h1> {props.course} </h1>
   )
 }
 
 const Parts = (props) => {
+  console.log(props)
   return(
-    <p> {props.part} {props.exercise} </p>
+    <p> {props.part.name} {props.part.exercises} </p>
   )
 }
 
 const Content = (props) => {
+  console.log(props)
   return(
     <div>
-      <Parts part={props.part1} exercise={props.exercises1} />
-      <Parts part={props.part2} exercise={props.exercises2} />
-      <Parts part={props.part3} exercise={props.exercises3} />
+      <Parts part={props.part1}/>
+      <Parts part={props.part2} />
+      <Parts part={props.part3} />
     </div>
   )
 }
@@ -31,18 +34,24 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name : 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name : 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name : 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3}/>
-      <Total exercises = {exercises1 + exercises2 + exercises3} />
+      <Content part1={part1} part2={part2} part3={part3}/>
+      <Total exercises = {part1.exercises + part2.exercises + part3.exercises} />
     </div>
   )
 }
